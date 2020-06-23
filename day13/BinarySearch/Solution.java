5e5bbcd4c6af8baadf5d8606e45c624860275010
jhpp114 <jhpp114@uw.edu>
2020-06-22 22:52:12 -0700

    Update Solution.java

--- day13/BinarySearch/Solution.java
+++ day13/BinarySearch/Solution.java
@@ -80,96 +80,7 @@ class Solution {
         }
         return findNode;
     }
-    // delete
-    public boolean deleteTreeNode(int deleteNode) {
-        Node current = root;
-        Node parent = null;
-        // search
-        boolean isLeft = true;
-        // define the location of the target
-        while (current.value != deleteNode) {
-            parent = current;
-            if (deleteNode < current.value) {
-                isLeft = true;
-                current = current.left;
-            } else {
-                isLeft = false;
-                current = current.right;
-            }
-            if (current == null) {
-                return false;
-            }
-        }
-        // if it is leaf
-        if (current.left == null && current.right == null) {
-            if (current == root) {
-                current = null;
-            }
-            else if (isLeft) {
-                parent.left = null;
-            } else {
-                parent.right = null;
-            }
-        }
-        // one child
-        else if (current.left == null) {
-            if (current == root) {
-                root = current.right;
-            }
-            else if (isLeft) {
-                parent.left = current.left;
-            } else {
-                parent.right = current.right;
-            }
-        }
-        // one child
-        else if (current.right == null) {
-            if (current == root) {
-                root = current.left;
-            } else if (isLeft) {
-                parent.left = current.left;
-            } else {
-                parent.right = current.right;
-            }
-        }
-        // both children
-        // get the replacement
-        else {
-            Node replaceNode = findMinNode(current);
-            if (current == root) {
-                root = replaceNode;
-            } else if (isLeft) {
-                parent.left = replaceNode;
-            } else {
-                parent.right = replaceNode;
-            }
-            // to connect the tree
-            replaceNode.left = current.left;
-        }
-        return true;
-    }
-    public Node findMinNode(Node _node) {
-        Node replaceNode = _node;
-        System.out.println("Current is: " + replaceNode);
-        Node rightNode = _node.right;
-        Node finalReplace = _node;
-        System.out.println("Current's right is: " + rightNode);
-        while (rightNode != null) {
-            // get the smallest
-            replaceNode = finalReplace;
-            finalReplace = rightNode;
-            rightNode = rightNode.left;
-            System.out.println(finalReplace);
-        }
-        if (_node.right != finalReplace) {
-            replaceNode.left = finalReplace.right;
-            finalReplace.right = _node.right; 
-        }
-        //finalReplace.right = replaceNode.right;
-        System.out.println("Smallest: " + finalReplace);
-        return finalReplace;
-    }
-
+    
     public static void main(String[] args) {
         Solution tree = new Solution();
         tree.addTree(50, "50");
@@ -188,18 +99,6 @@ class Solution {
         System.out.println("=======Look for 30==============");
         Node foundTree = tree.findNode(30);
         System.out.println(foundTree);
-        System.out.println("Test Pas=======================");
-        // System.out.println("=========Delete Node= 85==========");
-        // boolean isDeleted = tree.deleteTreeNode(85);
-        // System.out.println("Delete Succeses?: " + isDeleted);
-        System.out.println("Test pas=======================");
-        // System.out.println("============Delete Node 75 to test One Child");
-        // boolean isDeleted = tree.deleteTreeNode(75);
-        // System.out.println("Deleted Success?: " + isDeleted);
-        System.out.println("============Delete Two child ===============");
-        boolean isDeleted = tree.deleteTreeNode(25);
-        System.out.println("Deleted Success?: " + isDeleted);
-        tree.inorderTraverse(tree.root);
     }
 }
 
